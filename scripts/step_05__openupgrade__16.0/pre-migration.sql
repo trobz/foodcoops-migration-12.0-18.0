f6317e2e3787e046946f62b97df4cfeb8f6a24b6
Nguyen Minh Chien <chien@trobz.com>
2025-12-05 09:50:49 +0700

    [FIX] Error when computing the value of product_weight
    Issue: 7

--- scripts/step_05__openupgrade__16.0/pre-migration.sql
+++ scripts/step_05__openupgrade__16.0/pre-migration.sql
@@ -1,1 +1,5 @@
 
+UPDATE product_template SET weight_uom_id = (
+    SELECT res_id FROM ir_model_data
+    WHERE model='uom.uom' AND name='product_uom_kgm' AND module='uom'
+) WHERE weight_uom_id ISNULL;
