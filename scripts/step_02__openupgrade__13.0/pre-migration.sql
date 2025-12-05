abaeff4011644f5295b8edc24e8b5433c5e46349
Nguyen Minh Chien <chien@trobz.com>
2025-12-05 09:47:25 +0700

    [FIX] Step 13.0: Key (key)=(mail.bounce.alias) already exists
    Issue: 6

--- scripts/step_02__openupgrade__13.0/pre-migration.sql
+++ scripts/step_02__openupgrade__13.0/pre-migration.sql
@@ -5,3 +5,6 @@ DELETE FROM ir_mail_server;
 DELETE FROM fetchmail_server;
 DELETE FROM ir_attachment WHERE url LIKE '/web/content/%';
 DELETE FROM queue_job;
+
+UPDATE ir_config_parameter SET key = 'mail.bounce.alias.12.0'
+WHERE key = 'mail.bounce.alias';
--- scripts/step_07__openupgrade__18.0/post-migration.py
+++ scripts/step_07__openupgrade__18.0/post-migration.py
@@ -6,5 +6,20 @@ _logger.info("Executing post-migration.py script ...")
 env = env  # noqa: F821
 
 # Write custom script here
+bounce_alias_12 = env["ir.config_parameter"].search([
+    ("key", "=", "mail.bounce.alias.12.0")
+], limit=1)
+if bounce_alias_12:
+    bounce_alias = env["ir.config_parameter"].search([
+        ("key", "=", "mail.bounce.alias")
+    ], limit=1)
+    if bounce_alias:
+        # Reset value as it's from 12.0
+        _logger.info("Reset mail.bounce.alias value to %s ...", bounce_alias_12.value)
+        bounce_alias.value = bounce_alias_12.value
+        bounce_alias_12.unlink()
+    else:
+        # Else: reset the key as it's from 12.0
+        bounce_alias_12.key = "mail.bounce.alias"
 
 env.cr.commit()
