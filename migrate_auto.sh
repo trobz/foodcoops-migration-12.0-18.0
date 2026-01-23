7f085106817a00d876f44a0d8444eae4aafecbe6
Hoang Diep <mr.diephuuhoang@gmail.com>
2026-01-23 14:57:54 +0700

    fix auto migration script

--- migrate_auto.sh
+++ migrate_auto.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Get current date and time in format YYYYMMDD_HHMMSS
-# NOW=$(date +"%Y%m%d_%H%M%S")
-NOW=20251001
+NOW=$(date +"%Y%m%d_%H%M%S")
+# NOW=20251001
+
+# check if foodcoop_prod_$NOW exist, if not create it
+if [ ! -d foodcoop_prod_$NOW ]; then
+    echo "foodcoop_prod_$NOW does not exist"
+    mkdir foodcoop_prod_$NOW
+fi
 
 # restore v12 db into docker db
 pew in oow oow restoredb -d foodcoop12_prod_$NOW --database-path lalouve_production_latest.pgdump --database-format c --filestore-path foodcoop_prod_$NOW --filestore-format d
