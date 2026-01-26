9f780ecd3c2c5e6d7f0124a34a186109a520d695
Nguyen Minh Chien <chien@trobz.com>
2026-01-26 15:17:56 +0700

    [UPD] rename res_partner_account_move_line to partner_account_move_line

--- scripts/step_07__openupgrade__18.0/pre-migration.sql
+++ scripts/step_07__openupgrade__18.0/pre-migration.sql
@@ -170,7 +170,7 @@ WHERE name IN (
     -- 'purchase_compute_order',
     -- 'purchase_compute_order_min_package',
     -- 'purchase_package_qty',
-    -- 'res_partner_account_move_line',
+    'res_partner_account_move_line',
     'stock_inventory_barcode',
     'stock_inventory_barcode_custom',
     'stock_inventory_valuation_report',
