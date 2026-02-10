20e9a6d2907a5befd844353e40fa1a140ac02370
Hoang Diep <mr.diephuuhoang@gmail.com>
2026-02-10 15:22:54 +0700

    fix auto-clean db

--- migrate_auto.sh
+++ migrate_auto.sh
@@ -18,3 +18,17 @@ pew in oow oow upgrade --first-step 2 --last-step 8 --database foodcoop12_prod_$
 
 # dump the migrated db v18 into a file
 pew in oow oow dumpdb -d foodcoop12_prod_$NOW --database-path foodcoop12_migrated_$NOW.dump --database-format c --filestore-path foodcoop_prod_migrated_$NOW --filestore-format d
+
+pew in oow oow psql -c "SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE datname = 'foodcoop12_prod_$NOW'
+  AND pid <> pg_backend_pid();
+"
+# clean up and drop database foodcoop12_prod_$NOW
+pew in oow oow dropdb -d foodcoop12_prod_$NOW
+
+# clean up and drop filestore foodcoop_prod_migrated_$NOW
+rm -rf foodcoop_prod_migrated_$NOW
+
+# clean up and drop filestore foodcoop_prod_$NOW
+rm -rf foodcoop_prod_$NOW
