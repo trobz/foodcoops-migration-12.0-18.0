170c839631c81b93fe6d0b265d346eb9016db58d
Hoang Diep <mr.diephuuhoang@gmail.com>
2026-03-05 15:12:29 +0700

    update setup bundle based on current database name for each coop

--- scripts/step_07__openupgrade__18.0/pre-migration_setup.sql
+++ scripts/step_07__openupgrade__18.0/pre-migration_setup.sql
@@ -1,8 +1,10 @@
 
 -- Set the new modules to be installed
+-- bundle name is derived dynamically from the current database name prefix
+-- e.g. database "superquinquin_prod_20250305" → bundle "bundle_superquinquin"
 UPDATE ir_module_module
 SET state = 'installed'
 WHERE name IN (
-    'bundle_lalouve',
+    'bundle_' || split_part(current_database(), '_', 1),
     'pos_order_remove_line'
 )
