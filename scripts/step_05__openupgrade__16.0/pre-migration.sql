f0f50a26c9a059dd3ff9b7d2e8847cb4cf5ff989
Hoang Diep <mr.diephuuhoang@gmail.com>
2026-03-26 17:32:43 +0700

    update v16 docker

--- scripts/step_05__openupgrade__16.0/pre-migration.sql
+++ scripts/step_05__openupgrade__16.0/pre-migration.sql
@@ -1,5 +1,5 @@
 
-UPDATE product_template SET weight_uom_id = (
-    SELECT res_id FROM ir_model_data
-    WHERE model='uom.uom' AND name='product_uom_kgm' AND module='uom'
-) WHERE weight_uom_id ISNULL;
+-- UPDATE product_template SET weight_uom_id = (
+--     SELECT res_id FROM ir_model_data
+--     WHERE model='uom.uom' AND name='product_uom_kgm' AND module='uom'
+-- ) WHERE weight_uom_id ISNULL;
