87b1ed3b0a1ef327a73a9611b9be4395dac9b8e5
Nguyen Minh Chien <chien@trobz.com>
2026-04-02 17:47:36 +0700

    [UPD] product_packaging: qty = 1 when 0

--- scripts/step_07__openupgrade__18.0/pre-migration_purchase_package_qty.sql
+++ scripts/step_07__openupgrade__18.0/pre-migration_purchase_package_qty.sql
@@ -2,3 +2,7 @@ ALTER TABLE product_supplierinfo RENAME COLUMN package_qty TO mig_package_qty;
 -- ALTER TABLE account_move_line RENAME COLUMN package_qty TO mig_package_qty;
 ALTER TABLE purchase_order_line RENAME COLUMN package_qty TO mig_package_qty;
 -- ALTER TABLE stock_move RENAME COLUMN package_qty TO mig_package_qty;
+
+-- Update qty of product_packaging if it is 0 to 1 to avoid division by zero in the migration script.
+-- Lalouve product: [0391110] PAPIER OD BLANC A4 5RAMx500F
+UPDATE product_packaging SET qty = 1 WHERE qty = 0;
