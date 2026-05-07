3f7466c16fd899bd925aa0630d7aee3b9f34681e
Hoang Diep <mr.diephuuhoang@gmail.com>
2026-05-07 17:43:50 +0700

    fix for sqq: update noupdate for xmlid=        WHERE name='product_category_Souscriptions' and model='product.category';

--- scripts/step_08__regular__18.0/pre-migration.sql
+++ scripts/step_08__regular__18.0/pre-migration.sql
@@ -1,1 +1,25 @@
 
+-- Dispatcher: run database-specific SQL based on current database name prefix.
+DO $$
+DECLARE
+    db_prefix TEXT := split_part(current_database(), '_', 1);
+BEGIN
+
+    -- -------------------------------------------------------------------------
+    -- sqq
+    -- -------------------------------------------------------------------------
+    IF db_prefix = 'sqq' THEN
+
+        UPDATE ir_model_data
+        SET noupdate = 't'
+        WHERE name='product_category_Souscriptions' and model='product.category';
+
+    -- -------------------------------------------------------------------------
+    -- No match
+    -- -------------------------------------------------------------------------
+    ELSE
+        RAISE NOTICE 'No specific pre-migration SQL for prefix: %', db_prefix;
+
+    END IF;
+
+END $$;
