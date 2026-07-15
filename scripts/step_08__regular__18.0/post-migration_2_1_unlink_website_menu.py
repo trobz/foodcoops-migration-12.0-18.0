ce500fb772f52736d9c8aef3cbd9e7bcf6eafd31
Nguyen Minh Chien <chien@trobz.com>
2026-07-15 12:13:23 +0700

    [UPD] handle menu contactus in website_coop_custom

--- scripts/step_08__regular__18.0/post-migration_2_1_unlink_website_menu.py
+++ scripts/step_08__regular__18.0/post-migration_2_1_unlink_website_menu.py
@@ -7,23 +7,6 @@ env = globals().get("env")
 if env is None:
     raise RuntimeError("This script must be executed from oow with an injected Odoo env")
 
-def unlink_website_menu_by_url(env):
-    # Search all website menus with a URL in the input list and unlink them
-
-    # First check if the website.menu model exists in the current environment
-    if "website.menu" not in env:
-        _logger.warning("The model 'website.menu' does not exist in the current environment. Skipping unlinking website menus.")
-        return
-
-    candidate_menus = env["website.menu"].search([
-        ("url", "in", ["/contactus"]),
-    ])
-    fixed_count = 0
-    for menu in candidate_menus:
-        menu.unlink()
-        fixed_count += 1
-    _logger.info("Unlinked %d website menus", fixed_count)
-
 def deactivate_contactus_button(env):
     # Deactivate the view website.header_call_to_action if exists
     view = env["ir.ui.view"].search([("key", "=", "website.header_call_to_action")], limit=1)
@@ -31,7 +14,6 @@ def deactivate_contactus_button(env):
         view.active = False
         _logger.info("Deactivated view website.header_call_to_action")
 
-unlink_website_menu_by_url(env=env)
 deactivate_contactus_button(env=env)
 
 env.cr.commit()
